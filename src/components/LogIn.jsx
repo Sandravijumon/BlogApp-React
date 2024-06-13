8c4870742ce03dbc92ad346526a3d216ffddf2ed
Sandravijumon <sandravijumon15@gmail.com>
2024-06-13 14:22:07 +0530

    project created

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const LogIn = () => {
+    const [data,changeData]=useState(
+        {
+            "email":"",
+            "password":""
+        }
+    )
+    const inputHandler=(event)=>{
+        changeData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        
+        console.log(data)
+    }
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Email ID</label>
+                            <input type="text" className="form-control" name="email" value={data.email} onChange={inputHandler} />
+                        </div>
+
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input type="text" className="form-control"  name="password" value={data.password} onChange={inputHandler} />
+                        </div>
+
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success" onClick={readValue}>Log In</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LogIn
